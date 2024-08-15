97268706e4eb5eea5e457e5cd710b37e2f42a1f5
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2024-08-15 10:21:07 +0300

    How is the world ruled and led to war? Diplomats lie to journalists and believe these lies when they see them in print

--- print_address.c
+++ print_address.c
@@ -10,33 +10,42 @@
 
 int print_address(va_list ap)
 {
-	unsigned long addr = va_arg(ap, unsigned long);
+	void *arg_addr = va_arg(ap, void *);
+	unsigned long addr;
 	int n_chars = 0;
 	int cur_digit = 0;
 	int addr_digits[128];
 	int addr_len = 0;
 	int i = 0;
 
-	n_chars += write(STDOUT_FILENO, "0x", 2);
-	while (addr != 0)
+	if (arg_addr == NULL)
 	{
-		if (addr % 16 < 10)
-			addr_digits[i] = (addr % 16) + '0';
-		else
-			addr_digits[i] = (addr % 16) - 10 + 'a';
-		addr /= 16;
-		addr_len++;
-		i++;
+		n_chars += write(STDOUT_FILENO, "(nil)", 5);
 	}
-	addr_len--;
-	while (addr_len >= 0)
+	else
 	{
-		cur_digit = addr_digits[addr_len];
-		write(STDOUT_FILENO, &cur_digit, 1);
-		n_chars++;
+		addr = (unsigned long)(arg_addr);
+		n_chars += write(STDOUT_FILENO, "0x", 2);
+		while (addr != 0)
+		{
+			if (addr % 16 < 10)
+				addr_digits[i] = (addr % 16) + '0';
+			else
+				addr_digits[i] = (addr % 16) - 10 + 'a';
+			addr /= 16;
+			addr_len++;
+			i++;
+		}
 		addr_len--;
+		while (addr_len >= 0)
+		{
+			cur_digit = addr_digits[addr_len];
+			write(STDOUT_FILENO, &cur_digit, 1);
+			n_chars++;
+			addr_len--;
+		}
+		if (n_chars == 2)
+			n_chars += write(STDOUT_FILENO, "0", 1);
 	}
-	if (n_chars == 2)
-		n_chars += write(STDOUT_FILENO, "0", 1);
 	return (n_chars);
 }
